1710cc9bfdf85bd670b45d1972bc71e52edbabf8
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2025-08-04 21:00:14 +0000

    Updated 0002 +

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,7 @@ const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.SERVER_PORT || ALTER_PORT;
 
 const data = {
