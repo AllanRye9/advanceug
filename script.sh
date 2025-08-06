d313996f896a6e44b66de3331162493540aa2ec4
Oryema Allan <34004636+AllanRye9@users.noreply.github.com>
2025-08-06 12:38:17 +0000

    Recent edits to files

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ if [! git status --porcelain | grep -q '^\s*M' ]; then
 fi
   echo "There are modified files. Proceeding with the changes.";
   git add .; git commit -m "Recent edits to files" || { echo "Commit failed"; exit 1; };
-  git push || { echo "Push failed"; exit 1; };
+  git push -f || { echo "Push failed"; exit 1; };
   echo "Changes have been committed and pushed successfully.";
